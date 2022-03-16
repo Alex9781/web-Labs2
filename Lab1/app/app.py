b71e1f8cdb8d3e58b19fe3400654ee687c7bc0db
Alex Zmiev <alex.zmiev.az@gmail.com>
2022-03-16 18:52:50 +0300

    lab 1 template

--- Lab1/app/app.py
+++ Lab1/app/app.py
@@ -1,8 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 application = app
 
+
 @app.route('/')
 def index():
-    return "q!q"
+    msg = "!!!!1"
+    return render_template("index.html", message=msg)
+
+@app.route('/posts')
+def posts():
+    return render_template("posts.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")

d493dbe822db9c432d1a153d5055137802672461
Alex Zmiev <alex.zmiev.az@gmail.com>
2022-05-11 19:28:21 +0300

    lab 5

---
+++ Lab5/app/config.py
@@ -0,0 +1,5 @@
+SECRET_KEY = '02f910e01be8e5a37dee41260622557ef482def55cef805de1e58ba22d76afc0'
+MYSQL_USER = 'std_1558_lab4'
+MYSQL_DATABASE = 'std_1558_lab4'
+MYSQL_HOST = 'std-mysql.ist.mospolytech.ru'
+MYSQL_PASSWORD = 'Alexandr1'

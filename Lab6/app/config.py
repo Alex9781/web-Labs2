ea2357f7e5dfd90068314657da903fe553c4ba54
Alex Zmiev <alex.zmiev.az@gmail.com>
2022-05-18 19:08:36 +0300

    lab 6

---
+++ Lab6/app/config.py
@@ -0,0 +1,5 @@
+SECRET_KEY = '02f910e01be8e5a37dee41260622557ef482def55cef805de1e58ba22d76afc0'
+
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://std_1558_lab6:Alexandr1@std-mysql.ist.mospolytech.ru/std_1558_lab6'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ECHO = True
